48ca991d3a9584e95d92e415af96507ca660af72
Ivan Soto Fernandez <ivansotof@gmail.com>
2012-09-12 17:14:22 -0600

    first commit

---
+++ wp-auth.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+Plugin Name: WP Auth
+Plugin URI: http://www.ivansotof.com/
+Description: Auth functions for extending WP.
+Version: 1
+Author: Ivan Soto
+Author URI: http://www.ivansotof.com
+*/
